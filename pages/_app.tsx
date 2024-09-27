60105a331a98bef2604b0c4144d5c63392e91ad9
armin almuete <armin.almuete@acidgreen.com.au>
2024-09-27 21:12:03 +0800

    Fixed ESLINT issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Head>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-				/>
-				<title>My Next.js App</title>
-			</Head>
-			<Component {...pageProps} />
-		</>
-	);
+	return <Component {...pageProps} />;
 }
