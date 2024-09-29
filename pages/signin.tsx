c4b7bd29b90e284a0bbad5a1691fdfabd539eb6e
armin almuete <armin.almuete@acidgreen.com.au>
2024-09-29 18:23:04 +0800

    Added login

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -27,6 +27,11 @@ export default function SignIn() {
 		}
 	};
 
+	const [passwordType, setPasswordType] = useState("password");
+	const handleShowPassword = () => {
+		setPasswordType("text");
+	};
+
 	useEffect(() => {
 		const auth = getAuth();
 
@@ -110,7 +115,7 @@ export default function SignIn() {
 									<div className="relative flex items-center">
 										<input
 											name="password"
-											type="password"
+											type={passwordType}
 											value={password}
 											onChange={(e) => setPassword(e.target.value)}
 											required
@@ -124,6 +129,7 @@ export default function SignIn() {
 											stroke="#bbb"
 											className="w-4 h-4 absolute right-4 cursor-pointer"
 											viewBox="0 0 128 128"
+											onClick={handleShowPassword}
 										>
 											<path
 												d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
